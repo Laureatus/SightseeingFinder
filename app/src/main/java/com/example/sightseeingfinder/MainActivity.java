5d90dfd281e092850516a03df1b5999667bf4839
Laureatus <lorin.fankhauser@gmail.com>
2023-03-07 08:55:35 +0100

    Add osmdroid and add map to MainActivity view

--- app/src/main/java/com/example/sightseeingfinder/MainActivity.java
+++ app/src/main/java/com/example/sightseeingfinder/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.sightseeingfinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
-public class MainActivity extends AppCompatActivity {
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+public class MainActivity extends Activity implements LocationListener {
+    private MapView mMapView;
+    private MyLocationNewOverlay mMyLocationOverlay;
+    private LocationManager locationManager;
+
+    private double longitude;
+    private double latitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Initialisiere die Konfiguration von osmdroid
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
+        // Setze das Layout und die MapView
         setContentView(R.layout.activity_main);
+        mMapView = findViewById(R.id.mapview);
+
+        // Setze die Map-Konfiguration
+        mMapView.setTileSource(TileSourceFactory.MAPNIK);
+        mMapView.setMultiTouchControls(true);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5,this);
+            mMyLocationOverlay = new MyLocationNewOverlay(mMapView);
+            mMapView.getOverlays().add(mMyLocationOverlay);
+            IMapController mapController = mMapView.getController();
+            GeoPoint startPoint = mMyLocationOverlay.getMyLocation();
+            GeoPoint start = new GeoPoint(latitude, longitude);
+            mapController.setCenter(start);
+            mapController.setZoom(15.0);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},0);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMyLocationOverlay.enableMyLocation();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMyLocationOverlay.disableMyLocation();
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
     }
 }
